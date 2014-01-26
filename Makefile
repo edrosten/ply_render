862d23cbb4656fa79916694e4e2eb84849439bb6
Edward Rosten <ed@edwardrosten.com>
2014-01-26 15:31:10 +0000

    More

--- Makefile
+++ Makefile
@@ -3,22 +3,21 @@ CC=gcc
 
 debug=0
 
-ifeq ($(debug),0)
-CXXFLAGS=-Wall -Wextra -O3 -g -ggdb -std=c++0x 
-CXXFLAGS=-Wall -Wextra -O3 -g -ggdb -std=c++0x -DTOON_NDEBUG -DNDEBUG
-LDFLAGS=-lGVars3 -lcvd 
+CXXFLAGS=-g -O2 -Wall -Wextra -W -ansi -pedantic -std=c++11 -std=c++0x
+LDFLAGS= -lcvd
+
+ifeq ($(debug),1)
+CXXFLAGS+= -DTOON_NDEBUG -DNDEBUG -DTOON_CHECK_BOUNDS -D_GLIBCXX_DEBUG
 else
-CXXFLAGS=-Wall -Wextra -O0 -g -ggdb -std=c++0x -DTOON_CHECK_BOUNDS -D_GLIBCXX_DEBUG
-LDFLAGS=-lGVars3_debug -lcvd_debug -llapack
 endif
 
 default:render scanline_render
 
 render: render.o model_loader.o rply-1.1.1/rply.o runner.o
-	$(CXX) -o $@  $^ $(LDFLAGS) -lGL
+	$(CXX) -o $@  $^ $(LDFLAGS)
 
 scanline_render: scanline_render.o model_loader.o rply-1.1.1/rply.o  scanline_render_test.o
-	$(CXX) -o $@  $^ $(LDFLAGS) -lGL
+	$(CXX) -o $@  $^ $(LDFLAGS)
 
 clean:
 	rm -f *.o render scanline_render
--- runner.cc
+++ runner.cc
@@ -3,6 +3,7 @@
 
 #include "model_loader.h"
 #include "render.h"
+#include <TooN/SymEigen.h>
 #include <cvd/timer.h>
 #include <cvd/camera.h>
 #include <cvd/vector_image_ref.h>
@@ -11,20 +12,51 @@ using namespace TooN;
 using namespace CVD;
 using namespace std;
 
-int main(int, char** argv)
+
+int main(int argc, char** argv)
 {
-	Model m(argv[1]);
-	
-	ImageRef size(640,480);
+	int last=argc-1;
+	if(argc == 1)
+	{
+		cerr << "Please give PLY file as argument.\n";
+		exit(1);
+	}
+
+	//Set up an image size and camera model.
 	Camera::Linear cam;
-	cam.get_parameters()[0] = 500;
-	cam.get_parameters()[1] = 500;
+	ImageRef size(800, 600);
+
+	cam.get_parameters().slice<0,2>() = Ones * 400;
 	cam.get_parameters().slice<2,2>() = vec(size)/2;
+	
+	//Load the specified PLY file.
+	Model m(argv[last]);
 
+	
+	//Move the data to zero mean, unit variance just to make it easy for everything
+	//to appear in the image..
+	Vector<3> mean = Zeros;
+	Matrix<3> cross = Zeros;
+	for(auto v:m.vertices)
+	{
+		mean += v;
+		cross += v.as_col() * v.as_row();
+	}
+	mean /= m.vertices.size();
+	double scale = sqrt(SymEigen<3>(cross/m.vertices.size() - mean.as_col() * mean.as_row()).get_evalues()[0]);
+	for(auto&& v:m.vertices)
+		v = (v-mean)/scale;
 
-	SE3<> E = SE3<>::exp(makeVector(-.2,-.2,3,0,0,0));
+	//Set a suitable position and rottion.
+	SE3<> E = SE3<>::exp(makeVector(0.,0.,5.3,0,0,0));
 	E = E* SE3<>::exp(makeVector(0,0,0,.1,.5,.4));
 	
+
+	//Transform all the vertices.
+	vector<Vector<3>> c3d;
+	for(auto v:m.vertices)
+		c3d.push_back(E*v);
+
 	vector<EdgeSegment> a = render(E, m);
 
 	for(auto e:a)
